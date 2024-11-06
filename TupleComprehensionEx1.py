682f898165eb7ad587dc117f6daed470a015cb89
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-06 19:33:08 +0530

    TupleComprehensionEx1.py

--- TupleComprehensionEx1.py
+++ TupleComprehensionEx1.py
@@ -3,6 +3,6 @@ print("Enter List of Values separated by space:")
 x=(int(val) for val in input().split()) # feeling that It is tuple Comprehension
 #here x is called an object of <class,generator> 
 t=tuple(x) # Type Casting generator object into tuple object
-print("Elements in tuple=",t)
+print("Elements in tuple=",t) 
 print(type(t))
  
